89e1eb821f9cd7d3435d510551b3bc197760fc0d
vbangs <veronicabangs@gmail.com>
2021-07-02 21:39:21 -0400

    Import Bulma in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bulma/css/bulma.min.css';
 
 ReactDOM.render(
   <Router>
